31764d87dde0cfab34db35b214ffbe5935d14078
TransientTetra <letkijulian@gmail.com>
2023-03-24 15:04:23 +0100

    Move stuff around in nvim init; prettify it; add commented out plugins for the future

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,9 @@
+---------------------------------------
 -- Vim settings
+---------------------------------------
+vim.g.mapleader = ' '
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.o.hlsearch = false
 vim.wo.number = true
 vim.o.mouse = 'a'
@@ -13,16 +18,21 @@ vim.wo.signcolumn = 'yes'
 vim.o.termguicolors = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+vim.o.autowriteall = true
+vim.o.timeout = true
+vim.o.timeoutlen = 300
 
+---------------------------------------
 -- Keybindings
-vim.g.mapleader = ' '
+---------------------------------------
+vim.keymap.set({'n', 'v'}, '<Space>', '<Nop>', {silent = true})
 vim.keymap.set('n', '<C-s>', '<cmd>write<cr>', {desc = 'Save'})
 vim.keymap.set('n', '<C-_>', 'gcc', {desc = 'Toggle line comment', remap = true})
 vim.keymap.set('v', '<C-_>', 'gc', {desc = 'Toggle line comment', remap = true})
 
+---------------------------------------
 -- Plugins
+---------------------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -40,27 +50,36 @@ require("lazy").setup({
 	{'Mofiqul/vscode.nvim'},
 	{'nvim-lualine/lualine.nvim'},
 	{'numToStr/Comment.nvim'},
-	{"lukas-reineke/indent-blankline.nvim"},
-	-- {'akinsho/toggleterm.nvim'},
-	{'akinsho/bufferline.nvim'},
+	{'lukas-reineke/indent-blankline.nvim'},
+	{'akinsho/bufferline.nvim', dependencies = {'kyazdani42/nvim-web-devicons'}},
 	{'nvim-tree/nvim-tree.lua'},
 	{'lewis6991/gitsigns.nvim'},
-	{'kyazdani42/nvim-web-devicons'},
+	{'rmagatti/auto-session'},
+	-- {'glepnir/dashboard-nvim', dependencies = {'kyazdani42/nvim-web-devicons'}},
+	-- {'nvim-telescope/telescope.nvim', dependencies = {'nvim-lua/plenary.nvim'}},
+	-- {'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'},
+	-- {'akinsho/toggleterm.nvim', version = '*', config = true}
+	-- {'folke/which-key.nvim'},
 })
+
 require('lualine').setup({
 	options = {
 		component_separators = '|',
-		section_separators = { left = '', right = '' },
+		section_separators = {left = '', right = ''},
 	}
 })
 require('Comment').setup()
 require('indent_blankline').setup()
--- require('toggleterm').setup()
 require('bufferline').setup()
 require('nvim-tree').setup()
 require('gitsigns').setup()
 require('nvim-web-devicons').setup()
+require('auto-session').setup({
+	log_level = 'none',
+})
 
+---------------------------------------
 -- Color Scheme
+---------------------------------------
 vim.cmd.colorscheme('vscode')
 
