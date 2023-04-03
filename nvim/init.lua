d8764739e8f9d08a60d05bd74f240ffcffdc5f10
TransientTetra <letkijulian@gmail.com>
2023-04-03 16:58:19 +0200

    Add keymaps to LSP

--- nvim/init.lua
+++ nvim/init.lua
@@ -53,7 +53,7 @@ require("lazy").setup({
 	{ 'tpope/vim-sleuth' },
 	{ 'tiagovla/scope.nvim' },
 	{ 'ray-x/lsp_signature.nvim' },
-	{ 'akinsho/toggleterm.nvim',            version = '*',                                                            config = true },
+	{ 'akinsho/toggleterm.nvim',            version = '*',                                                              config = true },
 	{
 		'nvim-telescope/telescope-fzf-native.nvim',
 		build = 'make',
@@ -74,20 +74,20 @@ require("lazy").setup({
 		'VonHeikemen/lsp-zero.nvim',
 		dependencies = {
 			-- LSP Support
-			{ 'neovim/nvim-lspconfig' },       -- Required
-			{ 'williamboman/mason.nvim' },     -- Optional
+			{ 'neovim/nvim-lspconfig' },    -- Required
+			{ 'williamboman/mason.nvim' },  -- Optional
 			{ 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
 			-- Autocompletion
-			{ 'hrsh7th/nvim-cmp' },   -- Required
+			{ 'hrsh7th/nvim-cmp' }, -- Required
 			{ 'hrsh7th/cmp-nvim-lsp' }, -- Required
 			{ 'hrsh7th/cmp-buffer' }, -- Optional
-			{ 'hrsh7th/cmp-path' },   -- Optional
+			{ 'hrsh7th/cmp-path' }, -- Optional
 			{ 'saadparwaiz1/cmp_luasnip' }, -- Optional
 			{ 'hrsh7th/cmp-nvim-lua' }, -- Optional
 
 			-- Snippets
-			{ 'L3MON4D3/LuaSnip' },       -- Required
+			{ 'L3MON4D3/LuaSnip' },    -- Required
 			{ 'rafamadriz/friendly-snippets' }, -- Optional
 		}
 	},
@@ -222,6 +222,7 @@ require('nvim-treesitter.configs').setup({
 
 local lsp = require('lsp-zero').preset({
 	name = 'recommended',
+	set_lsp_keymaps = false,
 })
 -- lsp.ensure_installed({
 --     'autopep8',
@@ -238,12 +239,41 @@ local lsp = require('lsp-zero').preset({
 --     'stylua',
 -- })
 -- (Optional) Configure lua language server for neovim
-lsp.on_attach(function(user, bufnr)
+lsp.on_attach(function(_, bufnr)
 	local opts = { desc = '[F]ormat buffer', buffer = bufnr }
 
 	vim.keymap.set({ 'n', 'x' }, '<leader>f', function()
 		vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
 	end, opts)
+	local nmap = function(keys, func, desc)
+		if desc then
+			desc = 'LSP: ' .. desc
+		end
+
+		vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+	end
+
+	nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+	nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+	nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+	nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+	nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+	nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+	nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+	nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+	-- See `:help K` for why this keymap
+	nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+	nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+	-- Lesser used LSP functionality
+	nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+	nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+	nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+	nmap('<leader>wl', function()
+		print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	end, '[W]orkspace [L]ist Folders')
 end)
 lsp.nvim_workspace()
 lsp.setup()
