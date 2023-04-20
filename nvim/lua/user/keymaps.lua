a61ebee9cfecaeb794dbc0df251c76895b641873
TransientTetra <letkijulian@gmail.com>
2023-04-20 21:47:36 +0200

    Load keymaps for plugins only if said plugin is available

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,19 +1,17 @@
 ---------------------------------------
 -- Keymaps
 ---------------------------------------
--- Personal
+-- General
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.keymap.set('n', '<C-s>', '<cmd>write<cr>', { desc = 'Save' })
 vim.keymap.set('n', '<leader>w', '<cmd>write<cr>', { desc = 'Save' })
 vim.keymap.set('n', '<leader>q', '<cmd>confirm q<cr>', { desc = 'Quit' })
 vim.keymap.set('n', '<leader>]', '<cmd>bnext<cr>', { desc = 'Next tab' })
 vim.keymap.set('n', '<leader>[', '<cmd>bprev<cr>', { desc = 'Previous tab' })
-vim.keymap.set('n', '<C-b>', '<cmd>OverseerRun<cr>', { desc = 'Build tasks' })
 vim.keymap.set('i', '<C-h>', '<left>', { desc = 'Move cursor left' })
 vim.keymap.set('i', '<C-j>', '<down>', { desc = 'Move cursor down' })
 vim.keymap.set('i', '<C-k>', '<up>', { desc = 'Move cursor up' })
 vim.keymap.set('i', '<C-l>', '<right>', { desc = 'Move cursor right' })
-vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>', { desc = 'Toggle file explorer' })
 
 -- Splits
 vim.keymap.set('n', '|', '<cmd>vsplit<cr>', { desc = 'Vertical split' })
@@ -29,42 +27,63 @@ vim.keymap.set('n', '<C-Down>', '<cmd>resize +2<CR>', { desc = 'Resize split dow
 vim.keymap.set('n', '<C-Left>', '<cmd>vertical resize -2<CR>', { desc = 'Resize split left' })
 vim.keymap.set('n', '<C-Right>', '<cmd>vertical resize +2<CR>', { desc = 'Resize split right' })
 
+local IsAvailable = require('user.utils').IsAvailable
+-- Overseer
+if IsAvailable('overseer.nvim') then
+	vim.keymap.set('n', '<C-b>', '<cmd>OverseerRun<cr>', { desc = 'Build tasks' })
+end
+
+-- File explorer
+if IsAvailable('nvim-tree.lua') then
+	vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<cr>', { desc = 'Toggle file explorer' })
+end
+
 -- Session
-vim.keymap.set('n', '<leader>sb', require('session-lens').search_session, { desc = 'Browse saved sessions' })
-vim.keymap.set('n', '<leader>ss', require('auto-session').SaveSession, { desc = 'Save session' })
-vim.keymap.set('n', '<leader>sD', require('auto-session').DeleteSession, { desc = 'Delete current session' })
-vim.keymap.set('n', '<leader>sd', '<cmd>Autosession delete<cr>', { desc = 'Browse and delete a session' })
-vim.keymap.set('n', '<leader>sl', require('user.utils').RestoreLatestSession, { desc = 'Load most recent session' })
+if IsAvailable('session-lens') then
+	vim.keymap.set('n', '<leader>sb', require('session-lens').search_session, { desc = 'Browse saved sessions' })
+end
+if IsAvailable('auto-session') then
+	vim.keymap.set('n', '<leader>ss', require('auto-session').SaveSession, { desc = 'Save session' })
+	vim.keymap.set('n', '<leader>sD', require('auto-session').DeleteSession, { desc = 'Delete current session' })
+	vim.keymap.set('n', '<leader>sd', '<cmd>Autosession delete<cr>', { desc = 'Browse and delete a session' })
+	vim.keymap.set('n', '<leader>sl', require('user.utils').RestoreLatestSession, { desc = 'Load most recent session' })
+end
 
 -- Comment
-vim.keymap.set('n', '<C-_>', 'gcc', { desc = 'Toggle line comment', remap = true })
-vim.keymap.set('v', '<C-_>', 'gc', { desc = 'Toggle line comment', remap = true })
-vim.keymap.set('n', '<leader>/', 'gcc', { desc = 'Toggle line comment', remap = true })
-vim.keymap.set('v', '<leader>/', 'gc', { desc = 'Toggle line comment', remap = true })
+if IsAvailable('Comment.nvim') then
+	vim.keymap.set('n', '<C-_>', 'gcc', { desc = 'Toggle line comment', remap = true })
+	vim.keymap.set('v', '<C-_>', 'gc', { desc = 'Toggle line comment', remap = true })
+	vim.keymap.set('n', '<leader>/', 'gcc', { desc = 'Toggle line comment', remap = true })
+	vim.keymap.set('v', '<leader>/', 'gc', { desc = 'Toggle line comment', remap = true })
+end
 
 -- Telescope
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
-vim.keymap.set('n', '<leader>fb', function()
-	-- You can pass additional configuration to telescope to change theme, layout, etc.
-	require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-		winblend = 10,
-		previewer = false,
-	})
-end, { desc = 'Fuzzily search in current buffer' })
-vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Find files' })
-vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = 'Find help' })
-vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = 'Find current word' })
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = 'Find by grep' })
-vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = 'Find diagnostics' })
+if IsAvailable('telescope.nvim') then
+	vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+	vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+	vim.keymap.set('n', '<leader>fb', function()
+		-- You can pass additional configuration to telescope to change theme, layout, etc.
+		require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+			winblend = 10,
+			previewer = false,
+		})
+	end, { desc = 'Fuzzily search in current buffer' })
+	vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { desc = 'Find files' })
+	vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { desc = 'Find help' })
+	vim.keymap.set('n', '<leader>fw', require('telescope.builtin').grep_string, { desc = 'Find current word' })
+	vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { desc = 'Find by grep' })
+	vim.keymap.set('n', '<leader>fd', require('telescope.builtin').diagnostics, { desc = 'Find diagnostics' })
+end
 
 -- TroubleToggle
-vim.keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', '<leader>xl', '<cmd>TroubleToggle loclist<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', '<leader>xq', '<cmd>TroubleToggle quickfix<cr>', { silent = true, noremap = true })
-vim.keymap.set('n', 'gR', '<cmd>TroubleToggle lsp_references<cr>', { silent = true, noremap = true })
+if IsAvailable('trouble.nvim') then
+	vim.keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>', { silent = true, noremap = true })
+	vim.keymap.set('n', '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>', { silent = true, noremap = true })
+	vim.keymap.set('n', '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>', { silent = true, noremap = true })
+	vim.keymap.set('n', '<leader>xl', '<cmd>TroubleToggle loclist<cr>', { silent = true, noremap = true })
+	vim.keymap.set('n', '<leader>xq', '<cmd>TroubleToggle quickfix<cr>', { silent = true, noremap = true })
+	vim.keymap.set('n', 'gR', '<cmd>TroubleToggle lsp_references<cr>', { silent = true, noremap = true })
+end
 return {
 	lspKeymaps = function(bufnr)
 		vim.keymap.set({ 'n', 'x' }, '<leader>lf',
--- nvim/lua/user/utils.lua
+++ nvim/lua/user/utils.lua
@@ -8,4 +8,8 @@ M.RestoreLatestSession = function()
 		require('auto-session').RestoreSession(path:gsub('\\%%', '%%'))
 	end
 end
+M.IsAvailable = function(plugin)
+	local lazy_config_avail, lazy_config = pcall(require, "lazy.core.config")
+	return lazy_config_avail and lazy_config.plugins[plugin] ~= nil
+end
 return M
