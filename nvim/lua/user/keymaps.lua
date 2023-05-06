bc7262e28ab1d1f02b0191be23c9ae89fb64e97d
TransientTetra <letkijulian@gmail.com>
2023-05-06 11:12:49 +0200

    Move two keymaps to telescope section

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -59,8 +59,8 @@ end
 
 -- Telescope
 if IsAvailable('telescope.nvim') then
-	vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-	vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+	vim.keymap.set('n', '<leader>f?', require('telescope.builtin').oldfiles, { desc = 'Find recently opened files' })
+	vim.keymap.set('n', '<leader>f<space>', require('telescope.builtin').buffers, { desc = 'Find existing buffers' })
 	vim.keymap.set('n', '<leader>fb', function()
 		-- You can pass additional configuration to telescope to change theme, layout, etc.
 		require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
