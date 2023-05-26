349f75ed47cd5d7632e5d20394beb37e3335ea7a
TransientTetra <letkijulian@gmail.com>
2023-05-26 08:54:52 +0200

    Fix session lens button in dashboard

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -93,7 +93,7 @@ return {
 		-- }
 		dashboard.section.buttons.val = {
 			dashboard.button('l', '  Last Session', '<cmd>lua require(\'user.utils\').RestoreLatestSession()<cr>'),
-			dashboard.button('s', '  Saved Sessions', '<cmd>lua require(\'session-lens\').search_session()<cr>'),
+			dashboard.button('s', '  Saved Sessions', '<cmd>lua require(\'auto-session.session-lens\').search_session()<cr>'),
 			dashboard.button('n', '  New file', '<cmd>ene <BAR> startinsert <cr>'),
 			dashboard.button('f', '  Find File', '<cmd>Telescope find_files<cr>'),
 			dashboard.button('w', '  Find Word', '<cmd>Telescope live_grep<cr>'),
