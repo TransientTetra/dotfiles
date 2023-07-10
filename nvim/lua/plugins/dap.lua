31e1e35766f6e5663aac8110f9e924b48fcc1535
TransientTetra <letkijulian@gmail.com>
2023-07-10 15:47:08 +0200

    Add nvim-dap-virtual-text config

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -9,7 +9,22 @@ return {
 	{
 		'theHamsta/nvim-dap-virtual-text',
 		dependencies = { 'mfussenegger/nvim-dap', 'nvim-treesitter/nvim-treesitter' },
-		opts = {},
+		opts = {
+			enabled = true,            -- enable this plugin (the default)
+			enabled_commands = true,   -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+			highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+			highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+			show_stop_reason = true,   -- show stop reason when stopped for exceptions
+			commented = false,         -- prefix virtual text with comment string
+			only_first_definition = false, -- only show virtual text at first definition (if there are multiple)
+			all_references = true,     -- show virtual text on all all references of the variable (not only definitions)
+			-- experimental features:
+			virt_text_pos = "eol",     -- position of virtual text, see `:h nvim_buf_set_extmark()`
+			all_frames = true,         -- show virtual text for all stack frames not only current
+			virt_lines = false,        -- show virtual lines instead of virtual text (will flicker!)
+			virt_text_win_col = nil    -- position the virtual text at a fixed window column (starting from the first text column) ,
+			-- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+		},
 	},
 	{
 		'rcarriga/nvim-dap-ui',
