9cb297a602f228515472e8196ed87a5120ca4abe
TransientTetra <letkijulian@gmail.com>
2023-07-11 18:41:17 +0200

    Add a bunch of colorschemes and packs to astronvim config; change default colorscheme and disable format on save

--- astronvim/plugins/community.lua
+++ astronvim/plugins/community.lua
@@ -4,6 +4,17 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.vscode-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.gruvbox-nvim" },
+  { import = "astrocommunity.colorscheme.everforest" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  { import = "astrocommunity.colorscheme.kanagawa-nvim" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cmake" },
+--  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.lua" },
 }
