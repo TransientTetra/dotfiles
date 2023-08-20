2527f6a062b50b2db1b80ff8d3a8a12fa5ba5561
TransientTetra <letkijulian@gmail.com>
2023-08-20 18:09:33 +0200

    Remove dap icons from user config (stopped working)

--- astronvim/init.lua
+++ astronvim/init.lua
@@ -81,10 +81,5 @@ return {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
-    vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DapBreakpoint" })
-    vim.fn.sign_define("DapBreakpointCondition", { text = "󰟃", texthl = "DapBreakpoint" })
-    vim.fn.sign_define("DapBreakpointRejected", { text = "", texthl = "DapBreakpoint" })
-    vim.fn.sign_define("DapLogPoint", { text = "", texthl = "DapLogPoint" })
-    vim.fn.sign_define("DapStopped", { text = "", texthl = "DapStopped" })
   end,
 }
